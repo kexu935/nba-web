dabef03221e4991c608d55d409e2db78ebcbf8ae
kexu935 <xukekekekeke@gmail.com>
2018-04-14 15:50:15 -0700

    add spanner photo

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { AutoComplete, Input, Icon } from 'antd';
 import nba from 'nba';
+import {PROFILE_PIC_URL_PREFIX} from "../constants"
+
+const Option = AutoComplete.Option;
 
 export class SearchBar extends React.Component {
     state = {
@@ -9,7 +12,13 @@ export class SearchBar extends React.Component {
 
     handleSearch = (value) => {
         this.setState({
-            dataSource: !value ? [] : nba.searchPlayers(value).map(player => player.fullName)
+            dataSource: !value ? [] : nba.searchPlayers(value).map(({fullName, playerId}) => {
+                return (
+                    <Option key={playerId}>
+                        <img src={`${PROFILE_PIC_URL_PREFIX}/${playerId}.png`}/>
+                        <span>{fullName}</span>
+                    </Option>);
+            })
         });
     }
 
