8ed600b581b7a1f8fbd8100d6d29fc24a0cb6521
kexu935 <xukekekekeke@gmail.com>
2018-04-14 16:01:43 -0700

    fix bug

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,7 +14,7 @@ export class SearchBar extends React.Component {
         this.setState({
             dataSource: !value ? [] : nba.searchPlayers(value).map(({fullName, playerId}) => {
                 return (
-                    <Option key={playerId}>
+                    <Option key={playerId} value={fullName}>
                         <img className='player-option-image' src={`${PROFILE_PIC_URL_PREFIX}/${playerId}.png`}/>
                         <span className='player-option-label'>{fullName}</span>
                     </Option>);
@@ -36,6 +36,7 @@ export class SearchBar extends React.Component {
                 onSearch={this.handleSearch}
                 size="large"
                 placeholder="Search NBA Player"
+                optionLabelProp="value"
             >
                 <Input suffix={<Icon type="search" className="certain-category-icon" />} />
             </AutoComplete>
